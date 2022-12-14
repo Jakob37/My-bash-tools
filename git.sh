1cd71a6e7751f4dcd67778130180785bd257cbc5
Jakob Willforss <jakob.willforss@hotmail.com>
2022-12-14 09:51:01 +0100

    Pull and push

--- git.sh
+++ git.sh
@@ -3,9 +3,13 @@ alias bs="git branch; git status"
 alias p="git pull"
 alias co="git checkout"
 alias pm="git pull origin master"
+alias pmp="echo 'Pull and push'; git pull; git pull origin master; git push"
 alias s="git status"
 alias pu="git push"
+
 alias m="git checkout master"
+alias d="git checkout develop"
+
 alias dm="git difftool origin/master"
 alias c="git commit -m"
 alias a="git add -A ."
