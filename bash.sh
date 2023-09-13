df75cf8e4fe168fce16d4bf1eb6dcbc50446f10d
Jakob Willforss <jakob.willforss@hotmail.com>
2023-09-13 08:14:21 +0200

    lesslast

--- bash.sh
+++ bash.sh
@@ -4,4 +4,17 @@ alias ....="cd ../../.."
 alias ....="cd ../../../.."
 
 alias n="nautilus ."
+alias e="exit"
 
+function lessl {
+	path=$(ls $1 | tail -1)
+	less $1${path}
+}
+function taill {
+	path=$(ls $1 | tail -1)
+	tail $1${path}
+}
+function tailfl {
+	path = $(ls $@ | tail -1)
+	tail -f $1${path}
+}
